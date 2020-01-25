d08d3f7d0eb666f513ef99e60068e3edc8cf9658
Bora Shin <d6ymmd@gmail.com>
2020-01-25 21:59:33 +0900

    create views and templates

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import inventory_list
+from .views import inventory_list, inventory_create, inventory_detail, client_create, client_detail, client_list
 
 urlpatterns = [
     path('', inventory_list, name="inventory-list"),
+    path('create/', inventory_create, name="inventory-create"),
+    path('<int:pk>/', inventory_detail, name="inventory-detail"),
+    path('client/', client_list, name="client-list"),
+    path('client/create/', client_create, name="client-create"),
+    path('client/<int:pk>/', client_detail, name="client-detail"),
 ]
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -9,3 +9,30 @@ def inventory_list(request):
         'inventories': inventories,
     }
     return render(request, 'shop/inventory_list.html', context)
+
+
+def inventory_create(request):
+    return render(request, 'shop/inventory_create.html')
+
+
+def inventory_detail(request, pk):
+    inventory = Inventory.objects.get(id=pk)
+    return render(request, 'shop/inventory_detail.html', {'inventory': inventory})
+
+
+def client_create(request):
+    return render(request, 'shop/client_create.html')
+
+
+def client_detail(request, pk):
+    client = Client.objects.get(id=pk)
+    return render(request, 'shop/client_detail.html', {'client': client})
+
+
+def client_list(request):
+    clients = Client.objects.all()
+    context = {
+        'clients': clients,
+    }
+    return render(request, 'shop/client_list.html', context)
+
