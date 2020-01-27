dcafa0344fcc6ecf3a180ae816265245f4fdfbc1
Bora Shin <d6ymmd@gmail.com>
2020-01-27 20:42:29 +0900

    템플릿 수정

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=50, verbose_name='거래처명')
-    tel = models.CharField(max_length=20, verbose_name='거래처전화')
-    address = models.CharField(max_length=200, verbose_name='거래처주소')
+    name = models.CharField(max_length=50, verbose_name='이름')
+    tel = models.CharField(max_length=20, verbose_name='전화번호')
+    address = models.CharField(max_length=200, verbose_name='주소')
 
     def __str__(self):
         return self.name
@@ -13,10 +13,10 @@ class Client(models.Model):
 
 class Inventory(models.Model):
     name = models.CharField(max_length=50, verbose_name='제품명')
-    photo = models.ImageField(upload_to="", blank=True, verbose_name='제품사진')
-    desc = models.TextField(verbose_name='제품설명')
+    photo = models.ImageField(upload_to="", blank=True, verbose_name='제품 사진')
+    desc = models.TextField(verbose_name='제품 설명')
     price = models.PositiveIntegerField(verbose_name='가격')
-    stock = models.IntegerField(default=0, verbose_name='남은수량')
+    stock = models.IntegerField(default=0, verbose_name='남은 수량')
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='거래처')
 
     def __str__(self):
