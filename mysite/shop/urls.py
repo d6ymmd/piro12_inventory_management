7beae666d59bee1398313474a160fe62d6e9f49b
Bora Shin <d6ymmd@gmail.com>
2020-01-27 21:59:42 +0900

    수정, 삭제, 목록으로 가기 구현

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from .views import inventory_list, inventory_create, inventory_detail, client_create, client_detail, client_list
+from .views import inventory_list, inventory_create, inventory_detail, client_create, client_detail, client_list, \
+    inventory_edit, inventory_delete, client_edit, client_delete
 
 urlpatterns = [
     path('', inventory_list, name="inventory-list"),
     path('create/', inventory_create, name="inventory-create"),
     path('<int:pk>/', inventory_detail, name="inventory-detail"),
+    path('<int:pk>/edit/', inventory_edit, name="inventory-edit"),
+    path('<int:pk>/delete/', inventory_delete, name="inventory-delete"),
     path('client/', client_list, name="client-list"),
     path('client/create/', client_create, name="client-create"),
     path('client/<int:pk>/', client_detail, name="client-detail"),
+    path('client/<int:pk>/edit/', client_edit, name="client-edit"),
+    path('client/<int:pk>/delete/', client_delete, name="client-delete"),
 ]
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,5 +1,4 @@
-import re
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Inventory, Client
 from .forms import InventoryForm, ClientForm
@@ -12,13 +11,24 @@ def inventory_list(request):
 
 def inventory_create(request, inventory=None):
     if request.method == 'POST':
-        form = InventoryForm(request.POST, request.FILES)
+        form = InventoryForm(request.POST, request.FILES, instance=inventory)
         if form.is_valid():
             inventory = form.save()
             return redirect('inventory-list')
     else:
-        form = InventoryForm()
+        form = InventoryForm(instance=inventory)
     return render(request, 'shop/inventory_create.html', {'form': form, })
+
+
+def inventory_edit(request, pk):
+    inventory = get_object_or_404(Inventory, pk=pk)
+    return inventory_create(request, inventory)
+
+
+def inventory_delete(request, pk):
+    inventory = Inventory.objects.get(id=pk)
+    inventory.delete()
+    return redirect('inventory-list')
 
 
 def inventory_detail(request, pk):
@@ -28,13 +38,24 @@ def inventory_detail(request, pk):
 
 def client_create(request, client=None):
     if request.method == 'POST':
-        form = ClientForm(request.POST, request.FILES)
+        form = ClientForm(request.POST, request.FILES, instance=client)
         if form.is_valid():
             client = form.save()
             return redirect('client-list')
     else:
-        form = ClientForm()
+        form = ClientForm(instance=client)
     return render(request, 'shop/client_create.html', {'form': form, })
+
+
+def client_edit(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    return client_create(request, client)
+
+
+def client_delete(request, pk):
+    client = Client.objects.get(id=pk)
+    client.delete()
+    return redirect('client-list')
 
 
 def client_detail(request, pk):
